b4fbed0cab69c18400299940a791878ae4089cda
Kunal Ojha <kunalojha12@gmail.com>
2020-09-07 23:54:22 -0500

    adding background and sheep audio to app start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { timer } from 'rxjs/internal/observable/timer';
+import { Howl, Howler } from 'howler';
 
 
 @Component({
@@ -9,9 +10,34 @@ import { timer } from 'rxjs/internal/observable/timer';
 })
 export class AppComponent implements OnInit {
   count: number = 0;
+  sheep = new Howl({
+    src: ['../assets/audio/sheep.wav']
+  });
 
   ngOnInit() {
+    this.initApp();
+  }
+
+  initApp() {
+    this.startBackgroundAudio();
+    this.startSheepCounter();
+  }
+
+  private startSheepCounter() {
     timer(2500, 5000)
-      .subscribe(() => this.count++);
+      .subscribe(() => {
+        this.count++;
+        this.sheep.play();
+      });
+  }
+
+  private startBackgroundAudio() {
+    const sound = new Howl({
+      src: ['../assets/audio/night-time.mp3'],
+      autoplay: true,
+      loop: true
+    });
+
+    sound.play();
   }
 }
